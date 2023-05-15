a3ceb139dcd8da4ee6bd247d708c50a15de31473
Araf <arafsoloman3734@gmail.com>
2023-05-15 22:52:30 +0600

    modern react and redux app

---
+++ src/store/thunks/fetchUsers.js
@@ -0,0 +1,21 @@
+import { createAsyncThunk } from "@reduxjs/toolkit";
+import axios from 'axios';
+
+const fetchUsers = createAsyncThunk('users/fetch', async () => {
+    const response = await axios.get('http://localhost:3005/users');
+    // DEV ONLY
+    await pause(1000);
+
+    return response.data;
+});
+
+// Dev only
+
+const pause = (duration) => {
+    return new Promise((resolve) => {
+        setTimeout(resolve, duration);
+    });
+};
+
+
+export { fetchUsers };
